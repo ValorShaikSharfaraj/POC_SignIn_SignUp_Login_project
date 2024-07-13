8d55b54bf9cd7463df3593a36845dd2d24b3fa05
Shaik SHarfaraj Ahammed <codewithsharfaraj13@gmail.com>
2024-07-13 18:33:00 +0530

    Added Tailwind CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>ReactApp</h1>
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
